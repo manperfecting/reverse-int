95250d79dcd0b9546aa563423f96f427926e432a
Shahriyor <70831956+manperfecting@users.noreply.github.com>
2022-08-03 19:30:26 +0500

    feat: implement reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  var num = n.toString();
+  var arr = Array.from(num).reverse().join('');
+  if (num.substring(0, 1) == '-') {
+    return Number(Array.from(num.substring(1, num.length)).reverse().join(''));
+  }else{
+    return Number(arr);
+  }
 }
